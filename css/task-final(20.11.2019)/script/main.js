9bc38fc6e722f51c93865eba8e0ecf4f8a025abe
Jekos <jhonny.lbk@gmail.com>
2019-11-29 23:56:28 +0200

    right part

--- css/task-final(20.11.2019)/script/main.js
+++ css/task-final(20.11.2019)/script/main.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', () => {
-  const container = document.getElementById('left_grid');
-  const squares_quantity = 100;
+  const gridLeft = document.getElementById('left_grid');
+  const left_quantity = 100;
+  const gridRight = document.getElementById('right_grid');
+  const right_quantity = 60;
   const desr = `<div class="item-stats">
   <p class="name">Some Item Name</p>
   <p class="descr">Keep In Inventory To Gain Bonus</p>
@@ -11,12 +13,34 @@ window.addEventListener('load', () => {
   <p class="stat">Stat 4</p>
   </div> `;
 
-  for (let i = 0; i < squares_quantity; i++) {
-    const square = document.createElement('div');
-    square.innerHTML += desr;
-    
-    square.classList.add('grid-block');
-    container.appendChild(square);
-    console.log('created')
+  for (let i = 0; i < left_quantity; i++) {
+    const square1 = document.createElement('div');
+    square1.innerHTML += desr;
+
+    square1.classList.add('grid-block');
+    gridLeft.appendChild(square1);
+
+    switch (i) {
+      case 3:
+        square1.classList.add('box');
+        break;
+      case 60:
+        square1.classList.add('belt');
+        break;
+      case 59:
+        square1.classList.add('fire');
+        break;
+      case 36:
+        square1.classList.add('book');
+        break;
+    }
+  }
+
+  for (let i = 0; i < right_quantity; i++) {
+    const square2 = document.createElement('div');
+    square2.innerHTML += desr;
+
+    square2.classList.add('grid-block');
+    gridRight.appendChild(square2);
   }
 });
