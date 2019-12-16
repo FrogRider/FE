b1cfb76331457dbc87ca6b7f3ce777b0d6b3919c
Jekos <jhonny.lbk@gmail.com>
2019-12-16 02:22:54 +0200

    ai guesser (two variants)

--- js/task-3/script/main.js
+++ js/task-3/script/main.js
@@ -26,13 +26,17 @@ class Guesser {
 
   setup = () => {
     //TODO: check if playern typed number for ai to guess, then disable this input
-    this.num = Math.round(Math.random() * this.range[1]);
-    this.start.style.display = "none";
-    this.guess.style.display = "block";
-    this.input.style.pointerEvents = "initial";
-    this.rangeField.innerHTML = "Try to guess";
-    console.log(`Try to guess ${this.num}`);
-    console.log(`Ai's next move is ${Math.round((this.ai_range[1] - this.ai_range[0])/2)}`)
+    if (this.num_to_guess.value != '' ) {
+      this.num_to_guess.style.pointerEvents = "none";
+      this.num = Math.round(Math.random() * this.range[1]);
+      this.start.style.display = "none";
+      this.guess.style.display = "block";
+      this.input.style.pointerEvents = "initial";
+      this.rangeField.innerHTML = "Try to guess";
+      console.log(`Try to guess ${this.num}`);
+      console.log(`Ai's next move is ${Math.round((this.ai_range[1] - this.ai_range[0])/2)}`)
+    } else alert ('Сначала загадайте свое число')
+    
   };
 
   endGame = () => {
@@ -40,11 +44,18 @@ class Guesser {
     this.guess.style.display = "none";
     this.input.style.pointerEvents = "none";
     this.input.value = "";
+    this.box_for_nums.innerHTML = '';
     this.rangeField.innerHTML = "Try to guess";
+    this.num_to_guess.style.pointerEvents = "initial";
+    this.num_to_guess.value = '';
+    this.attemptsCounter = 0;
+    this.range = [0, 100];
+    this.ai_range = [0, 100];
   };
 
   ai = () => {
     let ai_guess = Math.round((this.ai_range[0] + this.ai_range[1])/2);
+    // let ai_guess = Math.floor(this.ai_range[0] + Math.random() * (this.ai_range[1] + 1 - this.ai_range[0]));
     if (ai_guess == num_to_guess.value) {
       alert(`Ai wins! Your number is ${ai_guess}`);
       this.endGame();
@@ -52,6 +63,8 @@ class Guesser {
       ai_guess < num_to_guess.value ? this.ai_range[0] = ai_guess : false;
         
       ai_guess > num_to_guess.value ? this.ai_range[1] = ai_guess : false;
+
+      this.box_for_nums.innerHTML += ` ${ai_guess}`
         
       }
       console.log(`ai says ${ai_guess}, [${this.ai_range[0]} , ${this.ai_range[1]}]`)
@@ -60,34 +73,36 @@ class Guesser {
   guesser = () => {
     
     let val = this.input.value;
-    if (val >= this.range[0] && val <= this.range[1]) {
-      this.ai();
-      this.attemptsCounter++;
-      if (val > this.num) {
-        this.range[1] = +val;
-        this.displayRange();
-        this.input.value = "";
-        alert("Загаданное число меньше");
-      }
-      if (val < this.num) {
-        this.range[0] = +val;
-        this.displayRange();
-        this.input.value = "";
-        alert("Загаданное число больше");
-      }
-      if (val === this.num) {
+    if (val !== '') {
+      if (val >= this.range[0] && val <= this.range[1]) {
+        this.ai();
+        this.attemptsCounter++;
+        if (val > this.num) {
+          this.range[1] = +val;
+          this.displayRange();
+          this.input.value = "";
+          alert("Загаданное число меньше");
+        }
+        if (val < this.num) {
+          this.range[0] = +val;
+          this.displayRange();
+          this.input.value = "";
+          alert("Загаданное число больше");
+        }
+        if (+val === this.num) {
+          alert(
+            `Вы угадали число. Количество попыток: ${this.getAttemptsCounter()}`
+          );
+          this.endGame();
+        }
+      } else {
         alert(
-          `Вы угадали число. Количество попыток: ${this.getAttemptsCounter()}`
+          `Загаданное число находится в диапазоне от ${this.range[0]} до ${
+            this.range[1]
+          }`
         );
-        this.endGame();
       }
-    } else {
-      alert(
-        `Загаданное число находится в диапазоне от ${this.range[0]} до ${
-          this.range[1]
-        }`
-      );
-    }
+    } else alert ('Type your number!')
   };
 }
 
